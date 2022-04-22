37183503592b881821b559a4cf99bd6bd172f433
elfaritsi <elmuhammad0209@gmail.com>
2022-04-22 14:55:01 +0700

    authentication implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
- 
+const AuthenticationError = require('../../exceptions/AuthenticationError');
+
 class UsersService {
   constructor() {
     this._pool = new Pool();
@@ -34,11 +35,11 @@ class UsersService {
       text: 'SELECT username FROM users WHERE username = $1',
       values: [username],
     };
- 
+
     const result = await this._pool.query(query);
- 
+
     if (result.rows.length > 0) {
-      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.')
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
 
@@ -47,7 +48,7 @@ class UsersService {
       text: 'SELECT id, username, fullname FROM users WHERE id = $1',
       values: [userId],
     };
-   
+
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
@@ -56,7 +57,27 @@ class UsersService {
 
     return result.rows[0];
   }
-  
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    return id;
+  }
 }
 
 module.exports = UsersService;
